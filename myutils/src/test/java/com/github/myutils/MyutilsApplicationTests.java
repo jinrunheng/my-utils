ba3f3f2ae14d2227da2eaa60c5d91c5a63cb8e5e
jinrunheng <1175088275@qq.com>
2023-02-07 17:06:39 +0800

    init project

---
+++ myutils/src/test/java/com/github/myutils/MyutilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.myutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyutilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
